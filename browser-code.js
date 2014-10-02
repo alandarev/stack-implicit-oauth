a2e8bbda75729dcec98085478734812a406a2579
alandarev <alandarev@gmail.com>
2014-10-02 19:44:20 +0100

    browser javascript code

---
+++ browser-code.js
@@ -0,0 +1,12 @@
+var bla = 1;
+var params = window.location.hash.substring(1);
+if (params && params.length > 0)  {
+  var xmlhttp=new XMLHttpRequest();
+  xmlhttp.onreadystatechange=function() {
+    if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+    }
+  }
+  xmlhttp.open("POST","keys",true);
+  xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+  xmlhttp.send(params);
+}
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var sio = require('./index.js');
+// Client ID, Callback
 sio.authorize(3604, authorized);
 
 function authorized(err, response)  {
   if(err) return console.log(err);
   console.log("authorized:");
-  console.log(response);
+  console.log(response.access_token);
 }
